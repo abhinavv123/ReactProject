b9fbe32d6ea9d2575e012ab77c5f2d5c24b99973
abhinavv123 <abgautam12@gmail.com>
2020-04-06 06:31:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 //import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 import Signup from './Components/Signup';
+//import Emp from './Components/emp';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Signup/>
     </div>
   );
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,118 +1,125 @@
 import React from 'react';
 import Hidden from './hidden.js';
-import datajson from '../data.json';
+//import datajson from '../data.json';
+import axios from 'axios';
 
 class Signup extends React.Component {
     constructor() {
         super();
         this.state = {
-            form:{
-                username:"",
-                password:""
+            form: {
+                username: "",
+                password: ""
             },
-            valid:{
-                formu:false,
-                formp:false,
-                forms:false,
-                formc:false,
-                formvalid:false
+            valid: {
+                formu: false,
+                formp: false,
+                forms: false,
+                formc: false,
+                formvalid: false
             },
-            message:""
+            message: ""
         }
     }
 
-    componentDidMount=()=>{
+    componentWillMount = () => {
+        console.log("will mount");
     }
-    
-    handlechange=(e)=>{
+
+    componentDidMount = () => {
+        console.log("mounted");
+    }
+
+    handlechange = (e) => {
         e.preventDefault();
-        let temp=this.state.form;
-        
-        if(e.target.id==="name")
-        {
-            temp.username=String(e.target.value);
-            this.setState({form:temp});
+        let temp = this.state.form;
+
+        if (e.target.id === "name") {
+            temp.username = String(e.target.value);
+            this.setState({ form: temp });
         }
-        else if(e.target.id==="password")
-        {
-            temp.password=String(e.target.value);
-            this.setState({form:temp});
+        else if (e.target.id === "password") {
+            temp.password = String(e.target.value);
+            this.setState({ form: temp });
         }
-        if(temp.username.length>0 && temp.password.length>0)
-        { 
-            let temp2=this.state.valid;
-            temp2.formvalid=true;
-            this.setState({valid:temp2});
+        if (temp.username.length > 0 && temp.password.length > 0) {
+            let temp2 = this.state.valid;
+            temp2.formvalid = true;
+            this.setState({ valid: temp2 });
         }
-        else
-        {
-            let temp2=this.state.valid;
-            temp2.formvalid=false;
-            this.setState({valid:temp2});
+        else {
+            let temp2 = this.state.valid;
+            temp2.formvalid = false;
+            this.setState({ valid: temp2 });
         }
     }
 
-    validate=(e)=>
-    {
+    validate = (e) => {
         e.preventDefault();
         console.log(e.target.value);
-        let temp=this.state.form;
-        let dataa=JSON.stringify(datajson);
-        let obj=JSON.parse(dataa);
-        //console.log(obj);
-        let found = obj.data.find((datta)=>
-        {
-            return ((temp.username===datta.username || temp.username===datta.email) && temp.password===datta.password);     
-        })
+        let temp = this.state.form;
+
+        // let dataa=JSON.stringify(datajson);
+        // let obj=JSON.parse(dataa);
         
-        if(!found)
-        {
-            this.setState({message:"Invalid email/username or password !"});
-        }
-        else
-        {
-            this.setState({message:""});
-        }
+        axios.get("http://localhost:4000/data").then((result) => {
+
+            let found = result.data.find((datta) => {
+                return ((temp.username === datta.username || temp.username === datta.email) && temp.password === datta.password);
+            })
+
+            if (!found) {
+                this.setState({ message: "Invalid email/username or password !" });
+            }
+            else {
+                this.setState({ message: "" });
+            }
+
+        }).catch((err) => {
+            console.log(err + " try again");
+        });
+
+
     }
 
-    hide=()=>{
-        let temp=this.state.valid;
-        temp.formc=true;
-        this.setState({valid:temp});
+    hide = () => {
+        let temp = this.state.valid;
+        temp.formc = true;
+        this.setState({ valid: temp });
     }
 
     render() {
         return (<div>
-            {this.state.valid.formc?<div><Hidden/></div>:<div>
-            <div className="container">
-                <div className="row" style={{margin:'100px'}}>
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">
-                                Sign In
+            {this.state.valid.formc ? <div><Hidden /></div> : <div>
+                <div className="container">
+                    <div className="row" style={{ margin: '100px' }}>
+                        <div className="col-md-8">
+                            <div className="card">
+                                <div className="card-header">
+                                    Sign In
                                 <button type="button" className="close" onClick={this.hide} aria-label="close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
 
-                            <div className="card-body">
-                                <form>
-                                    <div className="form-group">
-                                        <label htmlFor="name"><span style={{color:"grey"}}>Email or Username</span></label>
-                                        <input type="text" id="name" className="form-control" style={{width:'60%'}} onChange={this.handlechange} required></input>
-                                    </div>
+                                <div className="card-body">
+                                    <form>
+                                        <div className="form-group">
+                                            <label htmlFor="name"><span style={{ color: "grey" }}>Email or Username</span></label>
+                                            <input type="text" id="name" className="form-control" style={{ width: '60%' }} onChange={this.handlechange} required></input>
+                                        </div>
 
-                                    <div className="form-group">
-                                        <label htmlFor="password"><span style={{color:"grey"}}>Password</span></label>
-                                        <input type="password" id="password" className="form-control" style={{width:'60%'}} onChange={this.handlechange} required></input>
-                                    </div>
-                                    
-                                    <div className="form-group">
-                                        <input type="checkbox" name="stay" id="stay" value="stay" disabled></input>
-                                        <span style={{color:"grey"}}>&nbsp;stay signed-in</span>
-                                        <button className="btn btn-primary float-right" type="button" disabled={!this.state.valid.formvalid} onClick={this.validate}>Sign in</button>
-                                    </div>
-                                    <span className="text-danger">{this.state.message}</span>
+                                        <div className="form-group">
+                                            <label htmlFor="password"><span style={{ color: "grey" }}>Password</span></label>
+                                            <input type="password" id="password" className="form-control" style={{ width: '60%' }} onChange={this.handlechange} required></input>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <input type="checkbox" name="stay" id="stay" value="stay" disabled></input>
+                                            <span style={{ color: "grey" }}>&nbsp;stay signed-in</span>
+                                            <button className="btn btn-primary float-right" type="button" disabled={!this.state.valid.formvalid} onClick={this.validate}>Sign in</button>
+                                        </div>
+                                        <span className="text-danger">{this.state.message}</span>
 
                                     </form>
                                 </div>
@@ -121,11 +128,11 @@ class Signup extends React.Component {
                                         <button className="btn btn-light btn-lg btn-block" >Create account </button>
                                     </div>
                                 </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>}
+            </div>}
         </div>
         );
     }
